ec37f270be908e118c610b5d273a6985ef9d9183
Marc Pervaz Boocha <mboocha@sudomsg.xyz>
2023-08-19 17:44:45 +0530

    Create Menu component
    Signed-off-by: Marc Pervaz Boocha <mboocha@sudomsg.xyz>

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -54,6 +54,10 @@ function TabButton({ onClick, group }: { onClick: () => void, group: group }): J
   return <p onClick={onClick}>{group.name}</p>
 }
 
+function Hide({ children, show }: { children: any, show: boolean }) {
+  return show ? children : (<></>)
+}
+
 function Username({ onGroupCreate }: { onGroupCreate: (name: string) => void }): JSX.Element {
   const session = useSession()
   const [menuOpen, setMenuOpen] = useState(false);
@@ -85,8 +89,10 @@ function Username({ onGroupCreate }: { onGroupCreate: (name: string) => void }):
       <div className="flex items-center space-x-4">
         <img
           src="https://placekitten.com/250/250" // Replace with your actual image URL
+          width={40}
+          height={40}
           alt="User Avatar"
-          className="rounded-full w-10 h-10"
+          className="rounded-full"
         />
         <div>
           <p className="font-semibold">{session.data.user.name}</p>
@@ -94,65 +100,72 @@ function Username({ onGroupCreate }: { onGroupCreate: (name: string) => void }):
         </div>
       </div>
       <div className="relative inline-block text-left">
-        <button
-          type="button"
-          onClick={() => setMenuOpen(!menuOpen)}
-          className="rounded-md p-2 text-gray-800 hover:bg-white-100 focus:outline-none"
-          aria-label="Menu"
-        >
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
+        <Menu>
+          <button
+            onClick={() => open()}
+            className="block w-full px-4 py-2 text-gray-800 hover:bg-gray-100 text-left"
+            role="menuitem"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-        {menuOpen && (
-          <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
-            <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
-              <button
-                onClick={open}
-                className="block w-full px-4 py-2 text-gray-800 hover:bg-gray-100 text-left"
-                role="menuitem"
-              >
-                New Group
-              </button>
-              <Modal show={showCreateGroupModal}>
-              <ModalContent onClose={close} onCreate={handleCreateGroup}/>
-              </Modal>
-              <button
-                onClick={handleSignOut}
-                className="block w-full px-4 py-2 text-gray-800 hover:bg-gray-100 text-left"
-                role="menuitem"
-              >
-                Sign Out
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
-    </div>
+            New Group
+          </button>
+          <Modal show={showCreateGroupModal}>
+            <ModalContent onClose={close} onCreate={handleCreateGroup} />
+          </Modal>
+          <button
+            onClick={handleSignOut}
+            className="block w-full px-4 py-2 text-gray-800 hover:bg-gray-100 text-left"
+            role="menuitem"
+          >
+            Sign Out
+          </button>
+        </Menu >
+      </div >
+    </div >
   );
 }
 
-function 
+function Menu({ children }: { children: JSX.Element[] }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return <>
+    <button
+      type="button"
+      onClick={() => setMenuOpen(!menuOpen)}
+      className="rounded-md p-2 text-gray-800 hover:bg-white-100 focus:outline-none"
+      aria-label="Menu"
+    >
+      <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M4 6h16M4 12h16M4 18h16" />
+      </svg>
+    </button>
+    <Hide show={menuOpen} >
+      <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+        <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+          {children}
+        </div>
+      </div>
+    </Hide>
+  </>;
+}
 
 function Modal({ children, show }: { children: any, show: boolean }) {
   const [name, setName] = useState("");
-  return show ? (<div className="fixed inset-0 z-50 flex items-center justify-center" >
-    <div className="bg-white w-96 p-6 rounded-lg shadow-md">
-      {children}
+  return <Hide show={show}>
+    <div className="fixed inset-0 z-50 flex items-center justify-center" >
+      <div className="bg-white w-96 p-6 rounded-lg shadow-md">
+        {children}
+      </div>
     </div>
-  </div>
-  ) : (<></>)
+  </Hide>
 }
 
 function ModalContent({ onCreate, onClose }: { onCreate: (name: string) => void, onClose: () => void }) {
