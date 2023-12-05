159b89be3a793409795760751648d7aa87d16c2c
Marc Pervaz Boocha <mboocha@sudomsg.xyz>
2023-12-05 11:05:17 +0530

    Add upload button
    Signed-off-by: Marc Pervaz Boocha <mboocha@sudomsg.xyz>

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,7 +1,7 @@
-import {useState} from "react"
+import { useState } from "react"
 import Hide from "./Hide"
 //
-export default function Menu ({children}) {
+export default function Menu({ children }) {
 
 	const [
 		menuOpen,
@@ -14,18 +14,8 @@ export default function Menu ({children}) {
 			className="rounded-md p-2 text-secondary-text hover:bg-menu-hover focus:outline-none"
 			aria-label="Menu"
 		>
-			<svg
-				className="w-6 h-6"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					strokeLinecap="round"
-					strokeLinejoin="round"
-					strokeWidth={2}
-					d="M4 6h16M4 12h16M4 18h16" />
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+				<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 			</svg>
 		</button>
 		<Hide show={menuOpen}>
--- client/components/SendBox.jsx
+++ client/components/SendBox.jsx
@@ -1,21 +1,20 @@
-import {useState} from "react"
+import { useState } from "react"
 
-export default function SendBox ({onSend}) {
+export default function SendBox({ onSend }) {
 
-	const [
-		messageContent,
-		setMessageContent
-	] = useState("")
+	const [messageContent, setMessageContent] = useState("")
 
-	async function handleClick () {
-
+	async function handleClick() {
 		await onSend(messageContent)
 		setMessageContent("")
-
+	}
+	
+	async function handleUpload() {
+		await onSend("")
 	}
 
 	return (
-		<div className="grid grid-cols-[1fr,auto]">
+		<div className="grid grid-cols-[1fr,auto,auto]">
 			<input
 				type="text"
 				onChange={(e) => setMessageContent(e.target.value)}
@@ -23,8 +22,13 @@ export default function SendBox ({onSend}) {
 				value={messageContent}
 				className="resize-none border-2 h-fill grid-cols-1"
 			/>
-			<button className="rounded-full bg-secondary-bg p-1 grid-cols-2" onClick={handleClick}>send</button>
-		</div>
+			<button className="grids-cols-2" onClick={handleUpload}>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+					<path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
+				</svg>
+			</button>
+			<button className="rounded-full bg-secondary-bg p-1 grid-cols-3" onClick={handleClick}>send</button>
+		</div >
 	)
 
 }
