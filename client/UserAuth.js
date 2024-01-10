b895f586429abed6d29478286646f172c3ad1f27
Chryaor <learn.christyjose@gmail.com>
2024-01-10 09:10:20 +0530

    Added Vector clock and group map

--- client/UserAuth.js
+++ client/UserAuth.js
@@ -3,6 +3,104 @@ import { SocketInit } from "./SocketInit"
 import { openDB } from "idb"
 import { blobToBase64, Base64ToBlob } from "./Blob64.js"
 
+class GCounter {
+	constructor(iterable) {
+		this.counters = new Map(iterable);
+	}
+	increment(replica) {
+		this.counters.set(replica, (this.counters.get(replica) ?? 0) + 1)
+	}
+	get count() {
+		return [...this.counters.values()].reduce((sum, count) => sum + count, 0)
+	}
+	get state() {
+		return [...this.counters.entries()]
+	}
+	merge(otherCounter) {
+		otherCounter.counters.forEach((count, replica) => {
+			this.counters.set(replica, Math.max(this.counters.get(replica) ?? 0, count))
+		})
+	}
+}
+
+class VectorClock extends GCounter {
+	compare(otherClock) {
+		const allKeys = new Set([...this.counters.keys(), ...otherClock.counters.keys()]);
+		return [...allKeys].reduce((prev, replica) => {
+			const va = this.counters.get(replica) ?? 0;
+			const vb = otherClock.counters.get(replica) ?? 0;
+
+			switch (prev) {
+				case "equal":
+					return va > vb ? "greater" : va < vb ? "less" : prev;
+				case "less":
+					return va > vb ? "concurrent" : prev;
+				case "greater":
+					return va < vb ? "concurrent" : prev;
+				default:
+					return prev;
+			}
+		}, "equal");
+	}
+	delta(otherClock) {
+		const counter = new GCounter()
+		const allKeys = new Set([...this.counters.keys(), ...otherClock.counters.keys()])
+		allKeys.forEach(key => {
+			const value1 = this.counters.get(key) ?? 0
+			const value2 = otherClock.counters.get(key) ?? 0
+			counter.counters.set(key, value1 - value2)
+		})
+		return counter
+	}
+	clone() {
+		const ret = new VectorClock()
+		ret.merge(this)
+		return ret
+	}
+}
+
+class GroupMap {
+	constructor(userAuth) {
+		this.userAuth = userAuth
+	}
+	async loadVersion() {
+		const state = await this.userAuth.db.get("groupsVersion", 1)
+		this.version = new VectorClock(state)
+	}
+	get replicaId() {
+		return this.userAuth.clientID
+	}
+	async event(operation, params) {
+		const version = this.version.clone()
+		const event = { id: this.replicaId, version, OpId: crypto.randomUUID(), operation, ...params }
+		event.version.increment(event.id)
+		this.version.merge(event.version)
+		await this.userAuth.db.put("groupsVersion", this.version.state, 1)
+		await this.store(event)
+	}
+	async store(...events) {
+		await Promise.all(events.map(event => this.userAuth.db.add("groupsLog", { ...event, version: event.version.state })))
+		//To do: fix sort and replay
+		for (const event of events) {
+			switch (event.operation) {
+				case 'join':
+					this.userAuth.db.add("groups", event.group)
+					break
+				case 'leave':
+					await this.userAuth.db.delete("groups", event.groupId)
+					break
+			}
+		}
+		await this.userAuth.getGroups()
+	}
+	async joinGroup(group) {
+		await this.event("join", { group })
+	}
+	async leaveGroup(id) {
+		await this.event("leave", { groupId: id })
+	}
+}
+
 export class UserAuth {
 	constructor() {
 		this.connect = null
@@ -19,10 +117,12 @@ export class UserAuth {
 			await this.dbconnect()
 			var client = await this.db?.getAll("id")
 			if (client.length === 0) {
-				client = [crypto.getRandomValues(new Uint8Array(8)).toString()]
+				client = [crypto.randomUUID()]
 				await this.db.add("id", client[0], client[0])
 			}
 			this.clientID = client[0]
+			this.groupMap = new GroupMap(this)
+			this.groupMap.loadVersion()
 			this.connect = new SocketInit(this)
 			this.connect.socketMap.onRecieve = async message => await this.recieveNewMessage(message)
 			localStorage.setItem("token", value)
@@ -30,6 +130,7 @@ export class UserAuth {
 		}
 		else {
 			this.clientID = null
+			this.groupMap = null
 			this.connect?.close()
 			this.db?.close()
 			this.onSignOut?.()
@@ -101,12 +202,13 @@ export class UserAuth {
 	}
 
 	async addGroupCall(user, groupobjects) {
-		this.connect.sendNormal({ action: "join", user, group: groupobjects})
+		this.connect.sendNormal({ action: "join", user, group: groupobjects })
 	}
 
 	async addGroup(groupobjects) {
-		await this.db.add("groups", groupobjects)
-		await this.getGroups()
+		// await this.db.add("groups", groupobjects)
+		// await this.getGroups()
+		await this.groupMap.joinGroup(groupobjects)
 	}
 	async addUser(user, groupobjects) {
 		await this.db.put("groups", groupobjects)
@@ -124,12 +226,13 @@ export class UserAuth {
 	}
 
 	async deleteGroupCall(user, id) {
-		this.connect.sendNormal({ action: "leave", user, groupId: id})
+		this.connect.sendNormal({ action: "leave", user, groupId: id })
 	}
 
 	async deleteGroup(id) {
-		await this.db.delete("groups", id)
-		await this.getGroups()
+		//await this.db.delete("groups", id)
+		//await this.getGroups()
+		await this.groupMap.leaveGroup(id)
 	}
 	async getGroupMessages(groupId) {
 		const message = await this.db?.getAllFromIndex(
@@ -157,7 +260,7 @@ export class UserAuth {
 			"date": new Date(),
 			groupId: group.groupId,
 			parentId,
-			"messageId": crypto.getRandomValues(new Uint8Array(8)).toString()
+			"messageId": crypto.randomUUID()
 		}
 		await this.connect.socketMap.sendAllClients(data, ...group.users)
 		return data
