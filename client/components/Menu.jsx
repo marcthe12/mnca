2ea5cad57abc31d52b82ef0a40d2988f22472d66
Marc Pervaz Boocha <mboocha@sudomsg.xyz>
2024-01-22 19:25:22 +0530

    Add Heroicons

--- client/components/Menu.jsx
+++ client/components/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import Hide from "./Hide"
-//
+import Bars3Icon from '@heroicons/react/24/solid/Bars3Icon'
+
 export default function Menu({ children }) {
 
 	const [
@@ -14,9 +15,7 @@ export default function Menu({ children }) {
 			className="rounded-md p-2 text-secondary-text hover:bg-menu-hover focus:outline-none"
 			aria-label="Menu"
 		>
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-				<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-			</svg>
+			<Bars3Icon className="w-6 h-6"/>
 		</button>
 		<Hide show={menuOpen}>
 			<div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-menu-bg ring-1 ring-black ring-opacity-5">
--- client/components/SendBox.jsx
+++ client/components/SendBox.jsx
@@ -1,5 +1,7 @@
 import { useLayoutEffect } from "react"
 import { useRef, useState } from "react"
+import 	PaperClipIcon from "@heroicons/react/24/solid/PaperClipIcon"
+
 export default function SendBox({ onSend }) {
 
 	const [messageContent, setMessageContent] = useState("")
@@ -34,9 +36,7 @@ export default function SendBox({ onSend }) {
 
 			<input ref={fileInput} type="file" onChange={handleUpload} style={{ display: "none" }} />
 			<button className="grids-cols-2" onClick={() => fileInput.current.click()}>
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-					<path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
-				</svg>
+				<PaperClipIcon className="w-6 h-6" />
 			</button>
 			<button className="rounded-full bg-secondary-bg p-1 grid-cols-3" onClick={handleClick}>send</button>
 		</div >
